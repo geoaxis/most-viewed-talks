1e77f622d4468bde78bbcc08fed3593637c3cbca
Hatim aka geoaxis <72706+geoaxis@users.noreply.github.com>
2022-12-01 13:51:47 +0100

    fix the rum

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import '../styles/globals.css'
+
 import { ThemeProvider } from "@aws-amplify/ui-react";
 import { Amplify } from 'aws-amplify';
 
@@ -13,32 +15,34 @@ Amplify.configure(awsconfig);
 import { AwsRum } from 'aws-rum-web';
 
 
-try {
-  const config = {
-    sessionSampleRate: 1,
-    guestRoleArn: process.env.RUM_GUEST_ROLE_ARN,
-    identityPoolId: process.env.RUM_IDENTITY_POOL_ID,
-    endpoint: process.env.RUM_ENDPOINT,
-    telemetries: ["performance","errors","http"],
-    allowCookies: true,
-    enableXRay: false
-  };
 
-  const APPLICATION_ID = process.env.RUM_APPLICATION_ID;
-  const APPLICATION_VERSION = process.env.RUM_APPLICATION_VERSION;
-  const APPLICATION_REGION = process.env.RUM_APPLICATION_REGION;
-
-  const awsRum = new AwsRum(
-    APPLICATION_ID,
-    APPLICATION_VERSION,
-    APPLICATION_REGION,
-    config
-  );
-} catch (error) {
-  // Ignore errors thrown during CloudWatch RUM web client initialization
-}
 
 function MyApp({ Component, pageProps }) {
+
+  try {
+    const config = {
+      sessionSampleRate: 1,
+      guestRoleArn: "arn:aws:iam::518912499641:role/RUM-Monitor-eu-west-1-518912499641-8502404389661-Unauth",
+      identityPoolId: "eu-west-1:985897d8-acf4-4ad3-a595-da94149558f4",
+      endpoint: "https://dataplane.rum.eu-west-1.amazonaws.com",
+      telemetries: ["performance","errors","http"],
+      allowCookies: true,
+      enableXRay: false
+    };
+  
+    const APPLICATION_ID = '6fbeb63f-f0c5-4725-a14b-46ecbf6e87df';
+    const APPLICATION_VERSION = '1.0.0';
+    const APPLICATION_REGION = 'eu-west-1';
+  
+    const awsRum = new AwsRum(
+      APPLICATION_ID,
+      APPLICATION_VERSION,
+      APPLICATION_REGION,
+      config
+    );
+  } catch (error) {
+    // Ignore errors thrown during CloudWatch RUM web client initialization
+  }
   
   return (
     <ThemeProvider theme={studioTheme}>
