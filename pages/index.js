69a08c1bee35bf8eb6125af0ad6a37798a6086ca
Shahzada Hatim <72706+geoaxis@users.noreply.github.com>
2023-09-07 18:14:42 +0200

    adds conference name as variable

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ import styles from './VideoImage.module.css'
 const Home = (props) => {
   return (
     <>
-      <MainHeading title="Most viewed talks AWS reInvent 2022." subheading={`Last updated at  ${props.lastUpdated}(Stockholm/Sweden)`} />
+      <MainHeading title="Most viewed talks {process.env.CONFERENCE}." subheading={`Last updated at  ${props.lastUpdated}(Stockholm/Sweden)`} />
       <Divider></Divider>
 
       <YoutubeVideoCardCollection items={props.items}
