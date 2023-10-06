b1901a4d7bb613bee7b3813e90903162b04266a1
Hatim aka geoaxis <72706+geoaxis@users.noreply.github.com>
2023-10-06 16:05:11 +0200

    adds vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,16 @@ import { ThemeProvider } from "@aws-amplify/ui-react";
 import { Amplify } from 'aws-amplify';
 import "@aws-amplify/ui-react/styles.css";
 import { studioTheme } from "../src/ui-components";
+import { Analytics } from '@vercel/analytics/react';
+
 
 function MyApp({ Component, pageProps }) {
   
   return (
     <ThemeProvider theme={studioTheme}>
       <Component {...pageProps} />
+            <Analytics />
+
     </ThemeProvider>);
 }
 
