69d49a015ca833aaa9e23d606578f461620ae5ac
Shahzada Hatim <72706+geoaxis@users.noreply.github.com>
2023-12-05 11:48:08 +0100

    updates to multiple playlists

--- pages/index.js
+++ pages/index.js
@@ -35,66 +35,67 @@ let getMainDescription = (fullDescription) => {
   else return result;
 }
 export async function getStaticProps() {
-
   const youtube = google.youtube({
     version: 'v3',
     auth: process.env.GOOGLE_API_KEY
   });
 
-  let firstTime = true;
-  let hasNextToken = false;
-  let next = '';
+  const playlistIds = process.env.PLAYLIST.split(',');
   let dataMap = new Map();
 
-  while (firstTime || hasNextToken) {
-    firstTime = false;
+  for (const playlistId of playlistIds) {
+    let firstTime = true;
+    let hasNextToken = false;
+    let next = '';
 
-    const items = await youtube.playlistItems.list({
-      "part": [
-        "snippet,contentDetails"
-      ],
-      "maxResults": 50,
-      "pageToken": next,
-      "playlistId": process.env.PLAYLIST
-    });
+    while (firstTime || hasNextToken) {
+      firstTime = false;
 
+      const items = await youtube.playlistItems.list({
+        "part": [
+          "snippet,contentDetails"
+        ],
+        "maxResults": 50,
+        "pageToken": next,
+        "playlistId": playlistId
+      });
 
-    if (items.data.nextPageToken != null &&
-      (items.data.prevPageToken === undefined || items.data.nextPageToken != items.data.prevPageToken)) {
-      hasNextToken = true;
-      next = items.data.nextPageToken;
-    } else {
-      hasNextToken = false;
+      if (items.data.nextPageToken != null &&
+        (items.data.prevPageToken === undefined || items.data.nextPageToken != items.data.prevPageToken)) {
+        hasNextToken = true;
+        next = items.data.nextPageToken;
+      } else {
+        hasNextToken = false;
+      }
 
+      let ids = items.data.items?.reduce((a, c) => a.concat(String(c.contentDetails?.videoId)), new Array());
+
+      const result = await youtube.videos.list({
+        "part": [
+          "snippet,contentDetails,statistics,id"
+        ],
+        "id": ids
+      });
+      
+      result.data.items?.forEach(element => {
+        dataMap.set(element.id, element);
+      });
     }
-
-    let ids = items.data.items?.reduce((a, c) => a.concat(String(c.contentDetails?.videoId)), new Array())
-
-
-    const result = await youtube.videos.list({
-      "part": [
-        "snippet,contentDetails,statistics,id"
-      ],
-      "id": ids
-    });
-    result.data.items?.forEach(element => {
-      dataMap.set(element.id, element);
-    });
   }
 
-  const data = Array.from(dataMap.values()).sort((a, b) => Number(b.statistics?.viewCount) - Number(a.statistics?.viewCount))
-
+  const data = Array.from(dataMap.values()).sort((a, b) => Number(b.statistics?.viewCount) - Number(a.statistics?.viewCount));
 
   const lastUpdated = new Date().toLocaleString('sv-SE', {
     timeZone: 'Europe/Berlin',
-  })
+  });
 
-  let items = data?.reduce((a, c) => a.concat({ id: c.id, videoThumbnail: c.snippet.thumbnails.medium.url, videoTitle: c.snippet.title, videoDescription: getMainDescription(c.snippet.description), videoPlays: c.statistics.viewCount }), new Array())
+  const items = data?.reduce((a, c) => a.concat({ id: c.id, videoThumbnail: c.snippet.thumbnails.medium.url, videoTitle: c.snippet.title, videoDescription: getMainDescription(c.snippet.description), videoPlays: c.statistics.viewCount }), new Array());
 
   // Pass data to the page via props
-  return { props: { items, lastUpdated }, 
-           revalidate: 3600 // revalidate every hour 
-          }
+  return {
+    props: { items, lastUpdated },
+    revalidate: 3600 // revalidate every hour
+  };
 }
 
 
